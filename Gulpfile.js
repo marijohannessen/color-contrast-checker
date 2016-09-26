ce76ef396737b93e30e3ba887cfc848af03430c6
Mari Johannessen <mari.l.johannessen@gmail.com>
2016-09-26 16:31:23 -0500

    added gitignore

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var nano = require('gulp-cssnano');
+var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
 var browserSync = require('browser-sync');
 
 gulp.task('browser-sync', ['sass'], function() {
-    browserSync({
-        server: {
-            baseDir: ''
-        }
-    });
+  browserSync({
+      server: {
+          baseDir: ''
+      }
+  });
 });
 
 gulp.task('sass', function () {
@@ -23,17 +24,20 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('styles'));
 });
 
-// gulp.task('js', function () {
-//   return gulp.src('js/*.js')
-//       .pipe(uglify())
-//       .pipe(rename('script.min.js'))
-//       .pipe(browserSync.reload({stream:true}))
-//       .pipe(gulp.dest('js'))
-// });
+gulp.task('js', function () {
+  return gulp.src('js/script.js')
+      .pipe(babel({
+        presets: ['es2015']
+      }))
+      .pipe(uglify())
+      .pipe(rename('script.min.js'))
+      .pipe(browserSync.reload({stream:true}))
+      .pipe(gulp.dest('js'))
+});
 
 gulp.task('watch', function () {
     gulp.watch('styles/main.scss', ['sass']);
-    // gulp.watch('js/*.js', ['js']);
+    gulp.watch('js/script.js', ['js']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
