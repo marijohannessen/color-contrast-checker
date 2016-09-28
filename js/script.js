1b243e63702127c7c7319e7963750f4a82fe8bed
Mari Johannessen <mari.l.johannessen@gmail.com>
2016-09-27 23:56:23 -0500

    refactored js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 const hexToRgb = (hex) => {
-  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-      return r + r + g + g + b + b;
+    return r + r + g + g + b + b;
   });
-
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ? {
       r: parseInt(result[1], 16),
       g: parseInt(result[2], 16),
@@ -12,23 +11,23 @@ const hexToRgb = (hex) => {
   } : null;
 };
 
-function LightenDarkenColor(col, amt) {
-    var usePound = false;
-    if (col[0] == "#") {
-        col = col.slice(1);
-        usePound = true;
-    }
-    var num = parseInt(col,16);
-    var r = (num >> 16) + amt;
-    if (r > 255) r = 255;
-    else if  (r < 0) r = 0;
-    var b = ((num >> 8) & 0x00FF) + amt;
-    if (b > 255) b = 255;
-    else if  (b < 0) b = 0;
-    var g = (num & 0x0000FF) + amt;
-    if (g > 255) g = 255;
-    else if (g < 0) g = 0;
-    return (usePound?"#":"") + String("000000" + (g | (b << 8) | (r << 16)).toString(16)).slice(-6);
+const lightenDarkenColor = (col, amt) => {
+  let usePound = false;
+  if (col[0] == "#") {
+    col = col.slice(1);
+    usePound = true;
+  }
+  const num = parseInt(col,16);
+  let r = (num >> 16) + amt;
+  if (r > 255) r = 255;
+  else if  (r < 0) r = 0;
+  let b = ((num >> 8) & 0x00FF) + amt;
+  if (b > 255) b = 255;
+  else if  (b < 0) b = 0;
+  let g = (num & 0x0000FF) + amt;
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+  return (usePound?"#":"") + String("000000" + (g | (b << 8) | (r << 16)).toString(16)).slice(-6);
 }
 
 const rgbToHex = (rgb) => {
@@ -47,21 +46,20 @@ const checksRGB = (color) => {
   }
 }
 
+const getColorObject = (color) => {
+  const colorObj = {
+    r: checksRGB(color.r/255),
+    g: checksRGB(color.g/255),
+    b: checksRGB(color.b/255)
+  };
+  return colorObj;
+};
+
 const calculateRatio = (color1, color2) => {
-  const colorOneR = color1.r/255;
-  const colorOneG = color1.g/255;
-  const colorOneB = color1.b/255;
-  const colorTwoR = color2.r/255;
-  const colorTwoG = color2.g/255;
-  const colorTwoB = color2.b/255;
-  const colorOneRsRGB = checksRGB(colorOneR);
-  const colorOneGsRGB = checksRGB(colorOneG);
-  const colorOneBsRGB = checksRGB(colorOneB);
-  const colorTwoRsRGB = checksRGB(colorTwoR);
-  const colorTwoGsRGB = checksRGB(colorTwoG);
-  const colorTwoBsRGB = checksRGB(colorTwoB);
-  const colorOneL = ((0.2126 * colorOneRsRGB) + (0.7152 * colorOneGsRGB) + (0.0722 * colorOneBsRGB));
-  const colorTwoL = ((0.2126 * colorTwoRsRGB) + (0.7152 * colorTwoGsRGB) + (0.0722 * colorTwoBsRGB));
+  const colorOneObject = getColorObject(color1);
+  const colorTwoObject = getColorObject(color2);
+  const colorOneL = ((0.2126 * colorOneObject.r) + (0.7152 * colorOneObject.g) + (0.0722 * colorOneObject.b));
+  const colorTwoL = ((0.2126 * colorTwoObject.r) + (0.7152 * colorTwoObject.g) + (0.0722 * colorTwoObject.b));
   if (colorOneL > colorTwoL) {
       return ((colorOneL + 0.05)/(colorTwoL + 0.05));
   } else {
@@ -79,45 +77,35 @@ const contrast = (color) => {
   }
 }
 
-const colorOne = document.querySelector('#color1');
-const colorTwo = document.querySelector('#color2');
-const colorOneContainer = document.querySelector('.color1');
-const colorTwoContainer = document.querySelector('.color2');
-const colorOneTextBox = document.querySelector('.color1 .text-box');
-const colorTwoTextBox = document.querySelector('.color2 .text-box');
-const scoreBox = document.querySelector('.score');
-const colorOneRange = colorOneContainer.querySelector('.input-range');
-const colorTwoRange = colorTwoContainer.querySelector('.input-range');
-
 const updateAll = () => {
-  colorOneContainer.style.backgroundColor = colorOne.value;
-  colorTwoContainer.style.backgroundColor = colorTwo.value;
-  colorOneTextBox.style.backgroundColor = colorTwo.value;
-  colorOneTextBox.style.color = colorOne.value;
-  colorTwoTextBox.style.backgroundColor = colorOne.value;
-  colorTwoTextBox.style.color = colorTwo.value;
-  let colorOneHex = hexToRgb(colorOne.value);
-  let colorTwoHex = hexToRgb(colorTwo.value);
-  colorOneRange.style.color = contrast(colorOneHex);
-  colorTwoRange.style.color = contrast(colorTwoHex);
-  const message = scoreBox.querySelector('.message');
-  const verdict = scoreBox.querySelector('.verdict');
+  document.querySelector('.color1').style.backgroundColor = document.querySelector('#color1').value;
+  document.querySelector('.color2').style.backgroundColor = document.querySelector('#color2').value;
+  document.querySelector('.color1 .text-box').style.backgroundColor = document.querySelector('#color2').value;
+  document.querySelector('.color1 .text-box').style.color = document.querySelector('#color1').value;
+  document.querySelector('.color2 .text-box').style.backgroundColor = document.querySelector('#color1').value;
+  document.querySelector('.color2 .text-box').style.color = document.querySelector('#color2').value;
+  let colorOneHex = hexToRgb(document.querySelector('#color1').value);
+  let colorTwoHex = hexToRgb(document.querySelector('#color2').value);
+  document.querySelector('.color1').querySelector('.input-range').style.color = contrast(colorOneHex);
+  document.querySelector('.color2').querySelector('.input-range').style.color = contrast(colorTwoHex);
+  const message = document.querySelector('.score').querySelector('.message');
+  const verdict = document.querySelector('.score').querySelector('.verdict');
   if (calculateRatio(colorOneHex, colorTwoHex).toFixed(2) > 4.5) {
-    scoreBox.querySelector('.numbers').innerHTML = `Contrast Ratio: <span class="approved">${calculateRatio(colorTwoHex, colorOneHex).toFixed(2)}</span>`;
+    document.querySelector('.score').querySelector('.numbers').innerHTML = `Contrast Ratio: <span class="approved">${calculateRatio(colorTwoHex, colorOneHex).toFixed(2)}</span>`;
     verdict.textContent = `Yes, go for it.`;
-    message.innerHTML = `You can indeed use <span>${colorOne.value}</span> with <span>${colorTwo.value}</span>.<br><br><a href="#" class="verdict-link">Learn More</a>`;
+    message.innerHTML = `You can indeed use <span>${document.querySelector('#color1').value}</span> with <span>${document.querySelector('#color2').value}</span>.<br><br><a href="#" class="verdict-link">Learn More</a>`;
   } else if (calculateRatio(colorOneHex, colorTwoHex).toFixed(2) < 5.0 && calculateRatio(colorOneHex, colorTwoHex).toFixed(2) > 4.5) {
     scoreBox.querySelector('.numbers').innerHTML = `Contrast Ratio: <span class="approved">${calculateRatio(colorTwoHex, colorOneHex).toFixed(2)}</span>`;
     verdict.textContent = `Yes, but use with caution.`;
-    message.innerHTML = `You can use <span>${colorOne.value}</span> with <span>${colorTwo.value}</span>, but you're close to 4.5 so consider making some adjustments.<br><br><a href="#" class="verdict-link">Explain please?</a>`;
+    message.innerHTML = `You can use <span>${document.querySelector('#color1').value}</span> with <span>${document.querySelector('#color2').value}</span>, but you're close to 4.5 so consider making some adjustments.<br><br><a href="#" class="verdict-link">Explain please?</a>`;
   } else if (calculateRatio(colorOneHex, colorTwoHex).toFixed(2) < 4.5 && calculateRatio(colorOneHex, colorTwoHex).toFixed(2) > 4.0) {
     scoreBox.querySelector('.numbers').innerHTML = `Contrast Ratio: <span class="nope">${calculateRatio(colorTwoHex, colorOneHex).toFixed(2)}</span>`;
     verdict.textContent = `No, but very close.`;
-    message.innerHTML = `You <span>can't</span> use <span>${colorOne.value}</span> with <span>${colorTwo.value}</span>.<br />You're close to 4.5, so try a darker or lighter variation of a color.<br><br><a href="#" class="verdict-link">Explain please?</a>`;
+    message.innerHTML = `You <span>can't</span> use <span>${document.querySelector('#color1').value}</span> with <span>${document.querySelector('#color2').value}</span>.<br />You're close to 4.5, so try a darker or lighter variation of a color.<br><br><a href="#" class="verdict-link">Explain please?</a>`;
   } else {
     scoreBox.querySelector('.numbers').innerHTML = `Contrast Ratio: <span class="nope">${calculateRatio(colorTwoHex, colorOneHex).toFixed(2)}</span>`;
     verdict.textContent = `No.`;
-    message.innerHTML = `You <span>can't</span> use <span>${colorOne.value}</span> with <span>${colorTwo.value}</span>.<br><br><a href="#" class="verdict-link">Why not?</a>`;
+    message.innerHTML = `You <span>can't</span> use <span>${document.querySelector('#color1').value}</span> with <span>${document.querySelector('#color2').value}</span>.<br><br><a href="#" class="verdict-link">Why not?</a>`;
   }
 }
 
@@ -145,7 +133,6 @@ window.onload = () => {
     const inputValue = parseInt(input.value);
     let currentHex;
     input.addEventListener('focus', (evt) => {
-      console.log('hello');
       if (inputValue === 0) {
         currentHex = input.parentElement.parentElement.querySelector('.colorInput').value;
       }
@@ -161,13 +148,12 @@ window.onload = () => {
           value = 0 + (inputValue);
         }
       }
-      console.log(currentHex);
       if (inputValue > 0) {
-        newHex = LightenDarkenColor(currentHex, value);
+        newHex = lightenDarkenColor(currentHex, value);
       } else if (inputValue < 0) {
-        newHex = LightenDarkenColor(currentHex, value);
+        newHex = lightenDarkenColor(currentHex, value);
       } else if (inputValue === 0) {
-        newHex = LightenDarkenColor(currentHex, 0);
+        newHex = lightenDarkenColor(currentHex, 0);
       }
       input.parentElement.parentElement.querySelector('.colorInput').value = newHex;
       updateAll();
